9a00bd932fa9ebf828bffc4c40cef4d0d29d456e
macerno@hotmail.com <macerno@hotmail.com>
2022-04-21 15:51:01 -0300

    Versao Esqueleto

---
+++ src/main/java/br/edu/utfpr/magicpatobranco/MagicPatoBrancoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.magicpatobranco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicPatoBrancoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagicPatoBrancoApplication.class, args);
+    }
+
+}
