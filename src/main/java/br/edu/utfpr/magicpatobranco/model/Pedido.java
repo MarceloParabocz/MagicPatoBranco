8800e667d076bc1306668744695a3ad8ced7b74f
macerno@hotmail.com <macerno@hotmail.com>
2022-04-30 15:26:15 -0300

    feat: created model Pedido

---
+++ src/main/java/br/edu/utfpr/magicpatobranco/model/Pedido.java
@@ -0,0 +1,14 @@
+package br.edu.utfpr.magicpatobranco.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
